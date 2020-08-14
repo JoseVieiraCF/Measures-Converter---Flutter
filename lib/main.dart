6bbcf600693f189de74e7b7bc4d38ec23d5cd4de
José Vieira <josecarlos8161@gmail.com>
2020-08-14 16:31:15 -0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Measures Converter',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Measures Converter'),
+        ),
+        body: Center(
+          child: Text('Measures Converter'),
+        ),
+      ),
+    );
+  }
+}
+
+
